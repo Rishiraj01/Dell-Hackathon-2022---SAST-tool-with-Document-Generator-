e20cc3e7403160b20f09ff35e528c24e4a8f70df
Rishiraj01 <rishirajsharma231@gmail.com>
2022-11-03 23:20:45 +0530

    GUI interface added

---
+++ output.py
@@ -0,0 +1,24 @@
+import tkinter
+import PySimpleGUI as sg
+import os
+import sys
+
+working_directory = os.getcwd()
+
+layout = [  
+            [sg.Text("Choose a folder:")],
+            [sg.InputText(key="-FILE_PATH-"), 
+            sg.FolderBrowse(initial_folder=working_directory)],
+            [sg.Button('Submit'), sg.Exit()]
+        ]
+
+window = sg.Window("Integrated Architecture Document Generator", layout)
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    elif event == "Submit":
+        folder_address = values["-FILE_PATH-"]
+        os.system('bash')
+        os.system('ls')
+window.close()
