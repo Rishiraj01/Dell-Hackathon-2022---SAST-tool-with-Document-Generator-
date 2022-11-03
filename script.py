fcdda4d3f18778768d2a63f35bf1b58b70764612
bpstheg <19112002b@gmail.com>
2022-11-03 21:50:24 +0530

    More Organized File Output

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 from libsast import Scanner
-import pprint
 import sys
-import os
-from tabulate import tabulate
 import pandas as pd
+import streamlit as st
+from tabulate import tabulate
+import subprocess
 
-file_path = input('Enter a file path: ')
-pprint.pprint('Entered file path to be scanned: '+ file_path)
+pd.set_option('display.max_columns', None)
+
 options = {
     "ignore_filenames": {
         "bootstrap.min.js",
@@ -41,37 +41,108 @@ options = {
     },
     "show_progress": False,
 }
-paths = [file_path]
+paths = ["file/"]
 scanner = Scanner(options, paths)
 
 info = scanner.scan()
 
-choice = int(input("Enter 1 to create a results file : "))
-if(choice == 1):
-    sys.stdout = open('output2.txt','wt')
-    rules = []
+rules = []
 
-    data = info["semantic_grep"]['matches']
+data = info["semantic_grep"]['matches']
 
-    for a in info['semantic_grep']['matches']:
-        rules.append(a)
+for a in info['semantic_grep']['matches']:
+    rules.append(a)
 
-    rules_list = []
-    meta_list = []
+rules_list = []
 
-    for a in rules:
-        rules_list.append(list(data[a].values())[0])
-    
-    for a in rules:
-        meta_list.append(list(data[a].values())[1])
+meta_list = []
 
-    y = int(0)
-    sys.stdout = open('output2.txt','wt')
-    for i in rules_list:
+for a in rules:
+    rules_list.append(list(data[a].values())[0])
+
+for a in rules:
+    meta_list.append(list(data[a].values())[1])
+
+sys.stdout = open('output2.txt','wt')
+
+
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("\t\t\t\t\t\t\t\t\t\tIntegration Architecture Document")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+y = int(0)
+
+print("")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("\t\t\t\t\t\t\t\t\t\t\t\tINFO:")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("")
+y = 0
+db = False
+for i in rules_list:
+    if "INFO" in rules[y]:
         data = pd.DataFrame(i)
         df = data.loc[:, data.columns != 'file_path']
         pdtabulate = lambda df:tabulate(df,headers='keys',tablefmt='grid')
+        print("CODE DEPENDENCIES : {}".format(rules[y]))
         print(meta_list[y])
-        y = y + 1
         print(pdtabulate(df))
+        print("Total Dependencies : {}".format(df.shape[0]))
+        if 'SQL' in rules[y]:
+            db = True
+        if 'php' in rules[y]:
+            returned_output = subprocess.check_output(["php","-v"])
+            print("PHP Version : ",returned_output.decode("utf-8"))
         print("\n")
+    y = y + 1
+
+y = 0
+
+print("")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("\t\t\t\t\t\t\t\t\t\t\tRule Violation:")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("")
+if db:
+    print("You can only use Mongo as a Database")
+
+
+print("")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("\t\t\t\t\t\t\t\t\t\t\t\tWARNINGS:")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("")
+for i in rules_list:
+    if "WARNING" in rules[y]:
+        data = pd.DataFrame(i)
+        df = data.loc[:, data.columns != 'file_path']
+        pdtabulate = lambda df:tabulate(df,headers='keys',tablefmt='grid')
+        print("Vulnerability Detected : {}".format(rules[y]))
+        print(meta_list[y])
+        print(pdtabulate(df))
+        print("Count of possible Vulnerabilities : {}".format(df.shape[0]))
+        if 'php' in rules[y]:
+            returned_output = subprocess.check_output(["php","-v"])
+            print("PHP Version : ",returned_output.decode("utf-8"))
+        print("\n")
+    y = y + 1
+
+print("")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("\t\t\t\t\t\t\t\t\t\t\t\tERRORS:")
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("")
+y = 0
+for i in rules_list:
+    if "ERROR" in rules[y]:
+        data = pd.DataFrame(i)
+        df = data.loc[:, data.columns != 'file_path']
+        pdtabulate = lambda df:tabulate(df,headers='keys',tablefmt='grid')
+        print("Vulnerability Detected : {}".format(rules[y]))
+        print(meta_list[y])
+        print(pdtabulate(df))
+        print("Count of Vulnerabilities : {}".format(df.shape[0]))
+        if 'php' in rules[y]:
+            returned_output = subprocess.check_output(["php","-v"])
+            print("PHP Version : ",returned_output.decode("utf-8"))
+        print("\n")
+    y = y + 1
